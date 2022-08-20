23d0828d8cb2e047f84033bf23d954c3f62cedb8
yitianlian <91449279+yitianlian@users.noreply.github.com>
2022-08-20 14:04:39 +0800

    添加部分代码注释

--- python数学建模数据程序/03第3章  Python在高等数学和工程数学的应用(Python 程序及数据)/求积分.py
+++ python数学建模数据程序/03第3章  Python在高等数学和工程数学的应用(Python 程序及数据)/求积分.py
@@ -1,6 +1,6 @@
 #程序文件Pex3_8.py
 from sympy import *
 k,n=symbols('k  n')
-print(summation(k**2,(k,1,n)))
+print(summation(k**2,(k,1,n)))  # 求和（其实就是积分）
 print(factor(summation(k**2,(k,1,n))))  #把计算结果因式分解
 print(summation(1/k**2,(k,1,oo)))  #这里是两个小o表示正无穷
--- python数学建模数据程序/03第3章  Python在高等数学和工程数学的应用(Python 程序及数据)/泰勒展开.py
+++ python数学建模数据程序/03第3章  Python在高等数学和工程数学的应用(Python 程序及数据)/泰勒展开.py
@@ -1,8 +1,8 @@
 #程序文件Pex3_9.py
 from pylab import rc
 from sympy import *
-rc('font',size=16); rc('text',usetex=True)
+rc('font',size=16); rc('text',usetex=False)
 x=symbols('x'); y=sin(x)
-for k in range(3,8,2): print(y.series(x,0,k))  #等价于print(series(y,x,0,k))
+for k in range(3,8,2): print(y.series(x,0,k))  #等价于print(series(y,x,0,k))，泰勒展开到k-1项
 plot(y,series(y,x,0,3).removeO(),series(y,x,0,5).removeO(),
      series(y,x,0,7).removeO(),(x,0,2),xlabel='$x$',ylabel='$y$')
